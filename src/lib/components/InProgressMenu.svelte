c85fd6b086854da15b32fc5f8c8b46057068dc88
Kelamir <kelamir@protonmail.com>
2023-01-06 13:06:30 +0200

    Add a stop condition for the timer

--- src/lib/components/InProgressMenu.svelte
+++ src/lib/components/InProgressMenu.svelte
@@ -1,17 +1,22 @@
 <script>
-    import {inputMinutes} from "../../stores.js";
+    import {inputMinutes, isFinished} from "../../stores.js";
 
     let startTime = Date.now() / 1000; // in seconds
-    $: endTime = startTime + ($inputMinutes);
+    $: endTime = startTime + $inputMinutes;
     let minutes;
     let seconds;
     let timeRemaining;
-    setInterval(() => {
+    const interval =setInterval(() => {
         // has to count down
         const current = Date.now() / 1000;
         timeRemaining = endTime - current;
+        console.log(timeRemaining)
         minutes = Math.round(timeRemaining / 60);
         seconds = Math.round(timeRemaining % 60);
+        if (timeRemaining < 0) {
+          clearInterval(interval)
+          $isFinished = true;
+        }
     }, 1000)
 
 </script>
