4702265e6cb7a2c295553ead59c403aa31e610f5
Kelamir <kelamir@protonmail.com>
2023-01-06 11:23:52 +0200

    Add a few button tests

--- tests/test.ts
+++ tests/test.ts
@@ -10,3 +10,26 @@ test('button is present', async ({ page }) => {
 	await page.goto('http://localhost:5173');
 	expect(await page.getByRole('button').isVisible());
 })
+
+test('button is called Start', async ({ page }) => {
+	await page.goto('http://localhost:5173');
+	await expect(page.getByRole("button")).toContainText("Start");
+})
+test('button is called Stop after pressing it', async ({ page }) => {
+	await page.goto('http://localhost:5173');
+	await page.getByRole("button").click();
+	await expect(page.getByRole("button")).toContainText("Stop");
+})
+
+
+
+// test('button on toggle changes text from Start to Stop',
+// 	async ({ page }) => {
+// 	await page.goto('http://localhost:5173');
+//
+// 	const button = page.locator('Start');
+// 	await button.click();
+//
+// 	const toggledButton = page.locator('Stop');
+// 	expect(await toggledButton).toHaveText("");
+// })
