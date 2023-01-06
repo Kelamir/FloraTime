8fa32b60955dec9cf40bd7b6f77e9e8432c3d2f7
Kelamir <kelamir@protonmail.com>
2023-01-06 09:16:11 +0200

    Check for button visibility

--- tests/test.ts
+++ tests/test.ts
@@ -5,3 +5,8 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('http://localhost:5173');
 	expect(await page.textContent('h1')).toBe('FloraTime');
 })
+
+test('button is present', async ({ page }) => {
+	await page.goto('http://localhost:5173');
+	expect(await page.getByRole('button').isVisible());
+})
