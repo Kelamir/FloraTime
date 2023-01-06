53053ae8dd2edab0d1a0f47ef239daededd8dbed
Kelamir <kelamir@protonmail.com>
2023-01-06 12:56:22 +0200

    Set duration using a range slider

---
+++ src/lib/components/SetDuration.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { inputMinutes } from "../../stores.js";
+</script>
+
+<input type="range" bind:value={$inputMinutes} min="5" max="180" step="5">
+<p>Set duration: {$inputMinutes}</p>
