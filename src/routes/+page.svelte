2559ca623e4096bb462f996460cc13a722d896c8
Kelamir <kelamir@protonmail.com>
2023-01-09 11:37:07 +0200

    Improve code formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,22 @@
 <script>
-    import StartStopButton from "../lib/components/StartStopButton.svelte";
-    import SetDuration from "../lib/components/SetDuration.svelte";
-    import InProgressMenu from "../lib/components/InProgressMenu.svelte";
-    import FinishMenu from "../lib/components/FinishMenu.svelte";
-    import { timerState, timerStates } from "../stores.ts";
+  import StartStopButton from "../lib/components/StartStopButton.svelte";
+  import SetDuration from "../lib/components/SetDuration.svelte";
+  import InProgressMenu from "../lib/components/InProgressMenu.svelte";
+  import FinishMenu from "../lib/components/FinishMenu.svelte";
+  import { timerState, timerStates } from "../stores.ts";
 </script>
 
 <div>
-    <hr>
-    {#if $timerState === timerStates.Started}
-        <InProgressMenu />
-    {:else if $timerState === timerStates.Finished}
-        <FinishMenu />
-        <SetDuration />
-    {:else}
-        <SetDuration />
-    {/if}
-    <StartStopButton />
-    <hr>
+  <hr>
+  {#if $timerState === timerStates.Started}
+    <InProgressMenu />
+  {:else if $timerState === timerStates.Finished}
+    <FinishMenu />
+    <SetDuration />
+  {:else}
+    <SetDuration />
+  {/if}
+  <StartStopButton />
+  <hr>
 
 </div>
