2a1e9b0e922e48f810c5cdfc7f9fe184c8b76901
Kelamir <kelamir@protonmail.com>
2023-01-12 10:49:19 +0200

    Add auth0 related code

---
+++ src/routes/api/auth/signup/+server.ts
@@ -0,0 +1,6 @@
+import { handleRedirect } from '../../../../lib/auth';
+import type { RequestEvent } from "@sveltejs/kit";
+
+export function GET(request: RequestEvent): Response {
+  return handleRedirect(request, { screen_hint: 'signup' })
+}
