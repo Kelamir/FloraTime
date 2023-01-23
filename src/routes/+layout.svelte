266c4455a2b4ae8f4ee221f48e65a9acf4bff162
Kelamir <kelamir@protonmail.com>
2023-01-23 16:39:51 +0200

    Edit the styling a bit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,20 +6,25 @@
 
 </script>
 
-<h1>FloraTime</h1>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="/api/auth/signup">Sign up</a>
-  <a href="/api/auth/login">Log in</a>
-  <a on:click={removeCookies} href="/api/auth/logout">Log out</a>
-</nav>
+<h2 class="text-2xl font-bold underline first-letter:text-6xl first-letter:font-bold">Pomodoro timer</h2>
+
+<div class="flex py-5">
+  <a class="border bg-gray-300" href="/">Home</a>
+  <a class="border bg-gray-300" href="/api/auth/signup">Sign up</a>
+  <a class="border bg-gray-300" href="/api/auth/login">Log in</a>
+  <a class="border bg-gray-300 on:click={removeCookies}" href="/api/auth/logout">Log out</a>
+</div>
 
 
 {#if data.user}
   <img src={data.user.pictureUrl} alt="personal avatar" />
 {/if}
 
+<style lang="postcss">
+  :global(html) {
 
+}
+</style>
 
 <slot />
