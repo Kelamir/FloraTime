55f557a1c1d451d468dc56d2125d1a56a1bd6df4
Kelamir <kelamir@protonmail.com>
2023-01-25 11:24:44 +0200

    Convert navigation links to new Navbar element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,17 +8,21 @@
 
 </script>
 
-<NavbarLink href="https://duck.com">Duck</NavbarLink>
+
 <div class="flex flex-col items-center">
   <!-- <h2 class="text-2xl font-bold underline first-letter:text-6xl first-letter:font-bold">Pomodoro timer</h2> -->
 
   <div class="flex py-5 space-x-2">
+    <NavbarLink href="/">Home</NavbarLink>
+
     {#if data.user}
-      <a class="border bg-gray-300" on:click={removeCookies} href="/api/auth/logout">Log out</a>
+      <NavbarLink on:click={removeCookies} href="/api/auth/logout">Log out</NavbarLink>
+
     {:else}
-      <a class="border bg-gray-300" href="/api/auth/signup">Sign up</a>
-      <a class="border bg-gray-300" href="/api/auth/login">Log in</a>
+      <NavbarLink href="/api/auth/signup">Sign up</NavbarLink>
+      <NavbarLink href="/api/auth/login">Log in</NavbarLink>
     {/if}
+
   </div>
 
 
