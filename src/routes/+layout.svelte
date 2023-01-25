3dc4ed45abbf0b87cf0c9f1a37b37fd96a5858a6
Kelamir <kelamir@protonmail.com>
2023-01-25 17:05:20 +0200

    Trigger reactivity on custom element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,23 +10,15 @@
 
 
 <div class="flex flex-col items-center">
-  <!-- <h2 class="text-2xl font-bold underline first-letter:text-6xl first-letter:font-bold">Pomodoro timer</h2> -->
-
   <div class="flex py-5 space-x-2">
     <NavbarLink href="/">Home</NavbarLink>
-
     {#if data.user}
-       <!-- TODO make on:click work -->
       <NavbarLink on:click={removeCookies} href="/api/auth/logout">Log out</NavbarLink>
-
     {:else}
       <NavbarLink href="/api/auth/signup">Sign up</NavbarLink>
       <NavbarLink href="/api/auth/login">Log in</NavbarLink>
     {/if}
-
   </div>
-
-
 
   {#if data.user}
     <img src={data.user.pictureUrl} alt="personal avatar" />
