85e19be209f47816524515fc56fb40b82a5f1564
Kelamir <kelamir@protonmail.com>
2023-02-03 10:05:49 +0200

    Minimize markup

--- src/lib/components/ui/Navbarlink.svelte
+++ src/lib/components/ui/Navbarlink.svelte
@@ -1,3 +1,3 @@
-<a {...$$restProps} on:click class="bg-white  inline-flex items-center rounded border border-transparent px-9 py-1.5">
+<a {...$$restProps} on:click class="bg-white  inline-flex rounded">
     <slot />
 </a>
