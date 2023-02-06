77dfdb30576547ad656d26b02b75ccdb097800b1
Kelamir <kelamir@protonmail.com>
2023-02-06 18:34:54 +0200

    Use 12 grid layout for design
    It allows us to successfully align the navigation bar to the right
    part of the screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,17 +7,14 @@
 </script>
 
 
-<div class="flex flex-col items-center">
-  <Navbar data={data}/>
-
-  {#if data.user}
-    <img src={data.user.pictureUrl} alt="personal avatar" />
-  {/if}
-
-  <div class="bg-red-500 rounded p-5">
-    <slot />
+<div class="grid grid-cols-12 grid-rows-12">
+  <div class="row-start-1 col-start-8 pt-8 pb-16">
+    <Navbar data={data}/>
   </div>
 
+  <div class="row-start-2 col-start-2 col-span-10 bg-gray-700 p-5 rounded">
+      <slot />
+  </div>
 </div>
 
 
