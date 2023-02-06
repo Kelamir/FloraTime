5ec5a8f0afe45d801ccef20622e4dd2f80bacf06
Kelamir <kelamir@protonmail.com>
2023-02-06 18:28:45 +0200

    Remove + - buttons for regulating time for pom

--- src/lib/components/SetDuration.svelte
+++ src/lib/components/SetDuration.svelte
@@ -6,12 +6,3 @@
 
 <p class="text-8xl text-[#ffffff]">{$inputMinutes}:00</p>
 <!-- <input type="range" bind:value={$inputMinutes} min="5" max="240" step="5"> -->
-
-<div class="p-3">
-  <button on:click={() => {$inputMinutes = $inputMinutes - 5}} class="bg-white text-red inline-flex items-center rounded border border-transparent px-9 py-1.5 text-5xl">
-    -
-    </button>
-    <button on:click={() => {$inputMinutes = $inputMinutes + 5}} class="bg-white text-red inline-flex items-center rounded border border-transparent px-9 py-1.5 text-5xl">
-    +
-    </button>
-</div>
